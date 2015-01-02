e98d692807e54df38a84196b1baf9d9889fea06c
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2015-01-02 15:09:30 -0800

    Add a dEssentials compiler, fixes #20

---
+++ dEssentialsCompiler/dEssentialsCompiler/dEssentialsCompiler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace dEssentialsCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles(Environment.CurrentDirectory, "*.yml");
+            StringBuilder complete = new StringBuilder();
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i].ToLower().EndsWith("core.yml"))
+                {
+                    complete.Append(File.ReadAllText(files[i]));
+                }
+            }
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (!files[i].ToLower().EndsWith("core.yml"))
+                {
+                    complete.Append(File.ReadAllText(files[i]));
+                }
+            }
+            File.WriteAllText(Environment.CurrentDirectory + "/../dEssentials_Compiled.yml", complete.ToString());
+        }
+    }
+}
